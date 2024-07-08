1813cda1652a63a5eac45868c5699238f09c74cf
Aakashsusar <123715489+Aakashsusar@users.noreply.github.com>
2024-07-08 18:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let weightSlider = document.getElementById("myWeight");
+let weightOutput = document.getElementById("inputWeight");
+
+let heightSlider = document.getElementById("myHeight");
+let heightOutput = document.getElementById("inputHeight");
+
+weightOutput.innerHTML = weightSlider.value;
+heightOutput.innerHTML = heightSlider.value;
+
+weightSlider.oninput = function () {
+  weightOutput.innerHTML = this.value;
+}
+heightSlider.oninput = function () {
+  heightOutput.innerHTML = this.value;
+}
+
+function showValWeight(newVal) {
+  weightSlider.value=newVal;
+};
+
+function showValHeight(newVal) {
+  heightSlider.value=newVal;
+};
+
+weightSlider.addEventListener("input", updateValueWeight);
+heightSlider.addEventListener("input", updateValueHeight);
+function updateValueWeight(e) {
+  weightOutput.value = e.srcElement.value;
+}
+function updateValueHeight(e) {
+  heightOutput.value = e.srcElement.value;
+}
+
+function calculateBmi() {
+  let weight = document.bmiForm.realweight.value;
+  let height = (document.bmiForm.realheight.value)/100;
+  let realbmi = (weight)/Math.pow(height, 2);
+  let realbmiOutput = document.getElementById("yourbmi");
+  let messageOutput = document.getElementById("evaluationMessage");
+  let roundedBmi = realbmi.toFixed(1);
+  messageOutput.innerHTML = "";
+  realbmiOutput.innerHTML = " " + roundedBmi;
+  if (roundedBmi < 18.5) {
+    messageOutput.innerHTML = "Underweight (You need to see doctor..Gain your weight)";
+  } else if (roundedBmi< 25) {
+    messageOutput.innerHTML= "Normal weight (You are fit good...)";
+  } else if (roundedBmi< 30) {
+    messageOutput.innerHTML = "Overweight (Start Workout)";
+  } else {
+    messageOutput.innerHTML = "Obesse (You need to workout)";
+  }
+}
---
+++ script1.js
@@ -0,0 +1,9 @@
+function bmi(){
+    window.location.href="/BMI.html";
+}
+function Tdee(){
+    window.location.href="/TdeeCal.html";
+}
+function Cal(){
+    window.location.href="/Calorie.html";
+}
